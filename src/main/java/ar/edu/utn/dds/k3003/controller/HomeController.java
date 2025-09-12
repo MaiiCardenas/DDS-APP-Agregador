89ef6e2b118d95e9f85b1a0a6b140228e0c2ce55
Mailen Cárdenas <cardenasmailen@gmail.com>
2025-09-11 21:11:50 -0300

    agragando config postgres a las properties

--- src/main/java/ar/edu/utn/dds/k3003/controller/HomeController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/HomeController.java
@@ -2,12 +2,14 @@ package ar.edu.utn.dds.k3003.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<String> home() {
         return ResponseEntity.ok("La app está funcionando");
     }
